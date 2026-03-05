a8889e53397711a334924351a20ce78ad454efc1
silen-01 <dracon-dan@mail.ru>
2026-03-06 04:32:02 +0500

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { inject } from '@vercel/analytics'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import { language } from "@modules";
 import App from './App.tsx'
 import './index.css'
@@ -9,5 +10,6 @@ inject()
 createRoot(document.getElementById("root")!).render(
   <language.LanguageProvider>
     <App />
+    <SpeedInsights />
   </language.LanguageProvider>
 );
